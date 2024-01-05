44024d0c711b785c1b082bdf7efc7a98d472575e
Abishek Kumar <Abishek Kumar@DESKTOP-GDPLSQ8.home>
2024-01-05 14:23:53 +0100

    New class added by Abbie

---
+++ src/test/java/GoodEvening.java
@@ -0,0 +1,6 @@
+
+public class GoodEvening {
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+}
--- src/test/java/WelcomeBack.java
+++ src/test/java/WelcomeBack.java
@@ -3,5 +3,11 @@ public class WelcomeBack {
 public static void main(String[] args) {
 	System.out.println("welcomeBack");
 	System.out.println("Hello");
+	System.out.println("How are you");
+	int i=10;
+	int j = 20;
+	int z = i+j;
+	System.out.println(z);
+	System.out.println("hello hello");
 }
 }
